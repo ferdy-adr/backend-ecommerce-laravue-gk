c0e4ee2843b5664a3c919e85e7f04aa5c7003647
ferdy-adr <ferdypbg@gmail.com>
2021-12-08 14:14:16 +0700

    view product.index & ProductController@index

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -2,7 +2,10 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}"> <!--<![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!--<![endif]-->
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,14 +21,17 @@
 </head>
 
 <body>
-  {{-- sidebar --}}
-  @include('components.sidebar')  
+    {{-- sidebar --}}
+    @include('components.sidebar')
 
     <!-- Right Panel -->
     <div id="right-panel" class="right-panel">
 
-      {{-- navbar --}}
-      @include('components.navbar')  
+        {{-- navbar --}}
+        @include('components.navbar')
+
+        {{-- Breadcrumbs (optional) --}}
+        @yield('breadcrumbs')
 
         <!-- Content -->
         <div class="content">
@@ -39,4 +45,5 @@
     {{-- script --}}
     @include('components.script')
 </body>
+
 </html>
--- resources/views/pages/product/index.blade.php
+++ resources/views/pages/product/index.blade.php
@@ -1,5 +1,90 @@
 @extends('layouts.dashboard')
 
+@section('breadcrumbs')
+    <div class="breadcrumbs">
+        <div class="breadcrumbs-inner">
+            <div class="row m-0">
+                <div class="col-sm-4">
+                    <div class="page-header float-left">
+                        <div class="page-title">
+                            <h1>Product</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <div class="page-header float-right">
+                        <div class="page-title">
+                            <ol class="breadcrumb text-right">
+                                <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                                <li class="active">Product</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
 @section('content')
-    Product Index Page
+    <div class="orders">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title">List Product</strong>
+                        </div>
+                        <div class="table-stats order-table ov-h">
+                            <table class="table ">
+                                <thead>
+                                    <tr>
+                                        <th class="serial">#</th>
+                                        <th>Name</th>
+                                        <th>Category</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($items as $product)
+                                        <tr>
+                                            <td class="serial">{{ $product->id }}.</td>
+                                            <td> <span class="name">{{ $product->name }}</span> </td>
+                                            <td>{{ $product->categories_id }}</td>
+                                            <td> <span class="product">{{ $product->price }}</span> </td>
+                                            <td><span class="count">{{ $product->quantity }}</span></td>
+                                            <td>
+                                                <a href="#" class="btn btn-info btn-sm">
+                                                    <i class="fa fa-picture-o"></i>
+                                                </a>
+                                                <a href="#" class="btn btn-primary btn-sm">
+                                                    <i class="fa fa-pencil"></i>
+                                                </a>
+                                                <form action="#" method="POST" class="d-inline">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button class="btn btn-danger btn-sm">
+                                                        <i class="fa fa-trash"></i>
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="6" class="text-center p-5">
+                                                Data not found
+                                            </td>
+                                        </tr>
+
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div> <!-- /.table-stats -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
