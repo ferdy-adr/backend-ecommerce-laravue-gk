5a5cbc4c972e82fba7852abbd504c371973eff14
ferdy-adr <ferdypbg@gmail.com>
2021-12-16 13:15:41 +0700

    make model and relation Transaction, TransactionDetail

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Category;
-// use App\Models\ProductGallery;
-// use App\Models\TransactionDetail;
+use App\Models\ProductGallery;
+use App\Models\TransactionDetail;
 
 
 class Product extends Model
@@ -40,6 +40,6 @@ class Product extends Model
 
     public function transactionDetail()
     {
-        // return $this->hasMany(TransactionDetail::class);
+        return $this->hasMany(TransactionDetail::class, 'products_id');
     }
 }
---
+++ app/Models/TransactionDetail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Transaction;
+use App\Models\Product;
+
+class TransactionDetail extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'transactions_id',
+        'products_id'
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function transaction() {
+        return $this->belongsTo(Transaction::class, 'transactions_id', 'id');
+    }
+
+    public function product() {
+        return $this->belongsTo(Product::class, 'products_id', 'id');
+    }
+}
